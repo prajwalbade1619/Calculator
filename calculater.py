b111ffb5c5d40687dc3ae28f8e5294806bb7c233
prajwalbade1619 <156530669+prajwalbade1619@users.noreply.github.com>
2024-01-14 15:18:16 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,46 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero."
+
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+# Get user's choice of operation
+choice = input("Enter choice (1/2/3/4): ")
+
+# Perform calculation based on user's choice
+if choice in ('1', '2', '3', '4'):
+    if choice == '1':
+        result = add(num1, num2)
+        operation = '+'
+    elif choice == '2':
+        result = subtract(num1, num2)
+        operation = '-'
+    elif choice == '3':
+        result = multiply(num1, num2)
+        operation = '*'
+    elif choice == '4':
+        result = divide(num1, num2)
+        operation = '/'
+    
+    print(f"{num1} {operation} {num2} = {result}")
+else:
+    print("Invalid input. Please choose a valid operation (1/2/3/4).")
